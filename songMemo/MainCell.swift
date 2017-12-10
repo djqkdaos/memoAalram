a81632e23bb5dbe8a63884bec0969e84d2a5f263
L703 <l703@L703ui-iMac-3.local>
2017-12-10 21:03:14 +0900

    first commit

---
+++ songMemo/MainCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainCell.swift
+//  songMemo
+//
+//  Created by L703 on 2017. 12. 3..
+//  Copyright © 2017년 L703. All rights reserved.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ songMemo/dataModel.swift
@@ -0,0 +1,9 @@
+//
+//  dataModel.swift
+//  songMemo
+//
+//  Created by L703 on 2017. 12. 3..
+//  Copyright © 2017년 L703. All rights reserved.
+//
+
+import Foundation
